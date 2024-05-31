c88329da9e9a13508e5d720212cf367a4bd04291
Zz3r0 <83692237+AlienFukro@users.noreply.github.com>
2024-05-31 16:53:28 +0900

    Add files via upload

--- virtualbox_bk.bat
+++ virtualbox_bk.bat
@@ -5,3 +5,5 @@ VBoxManage controlvm "kali-linux-2024.1-virtualbox-amd64" poweroff
 SET YYYYMMDD=%DATE:/=% 
 
 VBOXManage export kali-linux-2024.1-virtualbox-amd64 -o "D:\OVF\kali\kali-linux-2024.1-virtualbox-amd64_%YYYYMMDD%.ova"
+
+VBoxManage startvm "kali-linux-2024.1-virtualbox-amd64"
